ff28d8d4a9a046c5d42418ec7d477fb061debee9
uz0000 <166586470+uz0000@users.noreply.github.com>
2024-05-29 10:23:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ df = pd.read_csv('cars_workshop.csv')
 st.header('Market of used cars data')
 st.write('Filter the data below to see the ads by manufacturer')
 
+#Filling NA values
+df['model_year'] = df['model_year'].fillna(df['model_year'].median())
+df['engine_capacity'] = df['engine_capacity'].fillna(df['engine_capacity'].median())
+df['odometer_value'] = df['odometer_value'].fillna(df['odometer_value'].median())
+
 df = df.drop(df.columns[0], axis=1)
 manufacturer_choice = df['manufacturer_name'].unique()
 selected_manu = st.selectbox('Select a manufacturer',manufacturer_choice)
